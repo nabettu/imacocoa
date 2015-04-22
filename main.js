3349b63ee97c76d1cb63dceff07ce97182454f80
tatsuakiw <tatsuaki.w@gmail.com>
2015-04-22 16:22:17 +0900

    自分の名前を送る機能実装

--- main.js
+++ main.js
@@ -1,24 +1,36 @@
 var milkcocoa = new MilkCocoa("https://io-ci8scyn06.mlkcca.com");
 var potisionDataStore = milkcocoa.dataStore("position");
 
-console.log(getQueryString().token);
-//アクセスユーザー固有のtokenを作成する
-if(!getQueryString().token){
-  var token = Math.random().toString(36).slice(-8);
-  window.location = "./index.html?token="+token;
+//初回アクセスの場合はアクセスユーザー固有のpage_idを作成する
+if(!getQueryString().page_id){
+  var page_id = Math.random().toString(36).slice(-20);
+  window.location = "./index.html?page_id="+page_id;
 }
 
 window.onload = function(){
+  //同じページを見ているユーザー一覧を読み込む
+
+  //地図の拡縮をユーザーが全員入る様に変更する
 }
 
-function sendControl(valueX,valueY){
-  potisionDataStore.push({positionX : valueX,positionY : valueY},function(data){
+function sendMyPosition(){
+  var userName = $("#name")[0].value;
+  //データ送信
+  potisionDataStore.push({userName : userName},function(data){
     console.log("milkcocoa送信完了!");
   });
 }
 
+//データ受信監視
 potisionDataStore.on("push",function(data){
   console.log(data.value);
+  //page_IDの照合
+
+  var userDom = document.createElement("li");
+  userDom.innerHTML = data.value.userName;
+  $("#userList").append(userDom);
+
+  //地図の拡縮をユーザーが全員入る様に変更
 });
 
 //URLの文字列を取得

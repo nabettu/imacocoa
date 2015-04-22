78458cdd468343d945ae3041a5404473982529df
tatsuakiw <tatsuaki.w@gmail.com>
2015-04-22 16:42:03 +0900

    gps送信

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ var milkcocoa = new MilkCocoa("https://io-ci8scyn06.mlkcca.com");
 var potisionDataStore = milkcocoa.dataStore("position");
 
 //初回アクセスの場合はアクセスユーザー固有のpage_idを作成する
-if(!getQueryString().page_id){
-  var page_id = Math.random().toString(36).slice(-20);
-  window.location = "./index.html?page_id="+page_id;
+if(!getQueryString().pageId){
+  var pageId = Math.random().toString(36).slice(-10);
+  window.location = "./index.html?pageId="+pageId;
 }
+
+var userId = Math.random().toString(36).slice(-5);
+var userName = "";
 
 window.onload = function(){
   //同じページを見ているユーザー一覧を読み込む
@@ -13,11 +16,25 @@ window.onload = function(){
   //地図の拡縮をユーザーが全員入る様に変更する
 }
 
-function sendMyPosition(){
-  var userName = $("#name")[0].value;
-  //データ送信
-  potisionDataStore.push({userName : userName},function(data){
-    console.log("milkcocoa送信完了!");
+function sendMyPosition(){  
+  userName = $("#name")[0].value;
+  
+  //位置情報取得
+  navigator.geolocation.watchPosition( function(position){
+      var lat=position.coords.latitude;
+      var lon=position.coords.longitude;
+
+      //データ送信
+      potisionDataStore.push({
+        pageId : getQueryString().pageId ,
+        userName : userName,
+        userId : userId,
+        lat : lat,
+        lon : lon
+      },
+        function(data){
+          console.log("milkcocoa送信完了!");
+        });
   });
 }
 

ccc7cc05e537b4cdfb9b6859a0e85f79aa2e04df
tatsuakiw <tatsuaki.w@gmail.com>
2015-04-24 14:06:11 +0900

    ua問題safari判定とios判定

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ var centerLat=35.5;
 var centerLon=139.6;
 
 window.onload = function(){
-  if (/twitter/.test(window.navigator.userAgent))$("#notifer").show();
+  if((!/safari/.test(window.navigator.userAgent))&&(/iphone|ipod|ipad/.test(window.navigator.userAgent)))$("#notifer").show();
   
    map = new GMaps({
                 div: "#map",
